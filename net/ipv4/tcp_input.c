ebaefed9e09f1e7a9449427ea7f476f5da43ccf5
Pzqqt <821026875@qq.com>
2018-08-12 22:28:19 +0800

    TCP input: Sender unilateral acceleration
    This method is very simple and violent,
    but it is said to reduce network packet loss
    and thus increase network speed.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -2294,7 +2294,7 @@ static void tcp_update_scoreboard(struct sock *sk, int fast_rexmit)
  */
 static inline void tcp_moderate_cwnd(struct tcp_sock *tp)
 {
-	tp->snd_cwnd = min(tp->snd_cwnd,
+	tp->snd_cwnd = max(tp->snd_cwnd,
 			   tcp_packets_in_flight(tp) + tcp_max_burst(tp));
 	tp->snd_cwnd_stamp = tcp_time_stamp;
 }
